a8134bb7111dc5836e172c7acac6096e71239a6a
adi8900 <adrianszymanski242@gmail.com>
2020-04-11 08:33:00 +0200

    davinci: Welcome POSP
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- potato_davinci.mk
+++ potato_davinci.mk
@@ -7,14 +7,12 @@
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit some common POSP stuff.
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_davinci
+PRODUCT_NAME := potato_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
